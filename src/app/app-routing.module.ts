eff99bac3ef5df9c41d17e7b4c877099985c1f6f
Khalid1592 <khalid.oulhamid@gmail.com>
2023-01-09 11:04:35 +0100

    Ajout de routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProduitComponent } from './produit/produit.component';
 
+
 const routes: Routes = [
   {path: 'produit', component:ProduitComponent},
   {path: 'dashboard', component:DashboardComponent},
--- src/app/service/produit.mock.service.ts
+++ src/app/service/produit.mock.service.ts
@@ -2,9 +2,11 @@ import { Injectable } from '@angular/core';
 import { Produit } from '../shared/produit';
 
 
-const PRODUITS: Produit[] = [{ref:'Livre',  quantite:50, prixUnitaire:20},
-{ref:'Cahier', quantite:200, prixUnitaire:5.25}, 
-{ref:'Stylo', quantite:500, prixUnitaire:2.10}];
+const PRODUITS: Produit[] = [
+                                {ref:'Livre',  quantite:50, prixUnitaire:20},
+                                {ref:'Cahier', quantite:200, prixUnitaire:5.25}, 
+                                {ref:'Stylo', quantite:500, prixUnitaire:2.10}
+                            ];
 
 @Injectable()
 export class ProduitMockService {
